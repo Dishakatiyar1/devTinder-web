fc9d404ffe49b5d12f21d4058cbb1601f001fd86
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-05-12 16:49:06 +0530

    api url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,7 @@
 // export const BASE_URL =
 //   location.hostname == "localhost" ? "http://localhost:8000" : "/api";
 
-// export const BASE_URL =
-//   location.hostname == "localhost"
-//     ? "http://localhost:8000"
-//     : "https://dev-connect-utfj.onrender.com";
-
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  location.hostname == "localhost"
+    ? "http://localhost:8000"
+    : "https://dev-connect-utfj.onrender.com";
