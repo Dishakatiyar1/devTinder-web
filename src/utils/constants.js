0d7cceccb0efc7f99ac201eb71e39b6c6f6c5b74
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-05-12 13:42:27 +0530

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
+// export const BASE_URL =
+//   location.hostname == "localhost" ? "http://localhost:8000" : "/api";
+
 export const BASE_URL =
   location.hostname == "localhost"
     ? "http://localhost:8000"
